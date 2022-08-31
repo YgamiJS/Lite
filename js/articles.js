f06efb6bbeb072b0b8ed2656da2af639585045b0
Дмитрий <105663726+YgamiJS@users.noreply.github.com>
2022-08-31 23:58:11 +0300

    Update articles.js

--- js/articles.js
+++ js/articles.js
@@ -4,5 +4,5 @@ const element = document.querySelector(".articles");
 element.addEventListener("click" , function(event){
     if(event.target.classList.contains("articles")) return;
     localStorage.setItem('data',JSON.stringify({title:event.target.textContent,page: event.target.getAttribute('value') }));
-    location.href = "file:///C:/Users/sahar/Desktop/Home/article.html";
+    location.href = "https://ygamijs.github.io/lite/articles.html";
 })
