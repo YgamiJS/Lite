c0fc056c37d3a4d778e15947d288f00d450c0b3f
Дмитрий <105663726+YgamiJS@users.noreply.github.com>
2022-09-01 01:47:28 +0300

    Update articles.js

--- js/articles.js
+++ js/articles.js
@@ -1,7 +1,6 @@
 "use strict";
 
-const element = document.querySelector(".articles");
-element.addEventListener("click" , function(event){
+document.querySelector(".articles").addEventListener("click" , function(event){
     if(event.target.classList.contains("articles")) return;
     localStorage.setItem('data',JSON.stringify({title:event.target.textContent,page: event.target.getAttribute('value') }));
     location.href = "https://ygamijs.github.io/Lite/article";
