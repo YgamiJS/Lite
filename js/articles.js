75fe8ff772d2f86df7a0f80152af336385d78fd9
Dima <dimasakharov@icloud.com>
2022-09-01 12:52:08 +0300

    seventh commit

--- js/articles.js
+++ js/articles.js
@@ -1,8 +1,7 @@
 "use strict";
 
-const element = document.querySelector(".articles");
-element.addEventListener("click" , function(event){
+document.querySelector(".articles").addEventListener("click" , function(event){
     if(event.target.classList.contains("articles")) return;
-    localStorage.setItem('data',JSON.stringify({title:event.target.textContent,page: event.target.getAttribute('value') }));
+    localStorage.setItem('data',JSON.stringify({title:event.target.textContent || "Что - то пошло не так :(",page: event.target.getAttribute('value') || "Что - то пошло не так :(" }));
     location.href = "file:///C:/Users/sahar/Desktop/Home/article.html";
 })
