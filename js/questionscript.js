83baf489aa084ae6de6cd35a19c34a76babc2f85
Dima <dimasakharov@icloud.com>
2022-09-04 12:55:23 +0300

    12 commit

--- js/questionscript.js
+++ js/questionscript.js
@@ -6,6 +6,6 @@ loadPage("question" , "pageValue");
 
 document.querySelector(".question").addEventListener("click" , function(event){
     if(event.target.classList.contains("question")) return;
-    localStorage.setItem("data-question" , { title:  event.target.getAttribute("value") , page: event.target.getAttribute("pageValue")})
+    localStorage.setItem("data-question" , JSON.stringify({ title:  event.target.getAttribute("value") , page: event.target.getAttribute("pageValue")}));
     location.href = "https://ygamijs.github.io/Lite/question.html";
 })
