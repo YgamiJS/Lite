a2ef7d11f03285b6103a4b42446f347c63a421e2
Dima <dimasakharov@icloud.com>
2022-09-05 17:02:47 +0300

    12 commit

--- js/articleContent.js
+++ js/articleContent.js
@@ -1,11 +1,11 @@
 "use strict";
 function GetDate(title , page , data ){
 document.addEventListener("DOMContentLoaded" , function(){
-    document.getElementById(title).textContent = document.title =  JSON.parse(localStorage.getItem(data)).title;
-    document.getElementById(page).textContent = JSON.parse(localStorage.getItem(data)).page;
+    document.querySelector(title).textContent = document.title =  JSON.parse(localStorage.getItem(data)).title;
+    document.querySelector(page).textContent = JSON.parse(localStorage.getItem(data)).page;
     localStorage.removeItem(data);
 })}
 
-GetDate("heading" , "content-page" , "data");
+GetDate("#heading" , "#content-page" , "data");
 
 export {GetDate};
--- js/questionscript.js
+++ js/questionscript.js
@@ -4,8 +4,8 @@ import { loadPage } from "./LoremText.js";
 
 loadPage("question" , "pageValue");
 
-document.querySelector(".question").addEventListener("click" , function(event){
-    if(event.target.classList.contains("question")) return;
+document.querySelector(".list-question").addEventListener("click" , function(event){
+    if(event.target.classList.contains("list-question")) return;
     localStorage.setItem("data-question" , JSON.stringify({ title:  event.target.getAttribute("value") , page: event.target.getAttribute("pageValue")}));
     location.href = "https://ygamijs.github.io/Lite/question.html";
 })
